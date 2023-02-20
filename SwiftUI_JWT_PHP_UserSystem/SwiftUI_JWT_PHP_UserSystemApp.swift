0b77825d5c3a5fcc9f6cad889853d54b6c196319
Iclementem <iclementem@outlook.es>
2023-02-20 10:07:55 +0100

    Initial Commit

---
+++ SwiftUI_JWT_PHP_UserSystem/SwiftUI_JWT_PHP_UserSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_JWT_PHP_UserSystemApp.swift
+//  SwiftUI_JWT_PHP_UserSystem
+//
+//  Created by Iván Clemente Moreno on 20/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_JWT_PHP_UserSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
